ed213535fb8e3fe44bbbdbb1eafe513b49f8ae7b
ysrajsingh <ysgahir1925@hotmail.com>
2025-05-17 15:20:09 +0530

    domains updated in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is pagead2.googlesyndication.com googleads.g.doubleclick.net ep2.adtrafficquality.google;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is pagead2.googlesyndication.com googleads.g.doubleclick.net ep2.adtrafficquality.google https://pagead2.googlesyndication.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
